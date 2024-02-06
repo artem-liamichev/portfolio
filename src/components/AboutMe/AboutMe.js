80d213bfa30c565deb29c1b59c00a9b6a1483119
Artem <a.v.lyamichev@gmail.com>
2024-02-06 14:37:55 -0300

    setup gh-pages

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -10,7 +10,7 @@ function AboutMe() {
                 <h3 className='about-me__subtitle'>Artem Liamichev</h3>
                 <p className='about-me__description'>Frontend developer, 32 y.o.</p>
                 <p className='about-me__text'>Software developer particularly focusing on Web (React, Vue) with the background in industrial control systems (PLC programming, HMI design, and project management).
-<br/>Since 2022, I have been into web development. Currently working in health-tech startup in Satiago, Chile.
+<br/>Since 2022 I have been into web development. Currently working in health-tech startup in Satiago, Chile.
 <br/>I like to listen to music and I'm also fond of running. Recently I ran my first half-marathon.</p>
                 <a className="about-me__link link" href="https://github.com/artem-liamichev" rel="noreferrer" target="_blank">GitHub</a>
             </div>
